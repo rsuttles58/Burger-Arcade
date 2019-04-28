33b6bbfa366bdfa74e0a39bc62bad6df9df65d5a
rsuttles58 <robert.suttles58@gmail.com>
2019-04-27 21:07:12 -0500

    Building MVC format

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,33 @@
 var connection = require("../config/connection.js");
 
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
+function objToSql(ob) {
+  var arr = [];
+
+  for (var key in ob) {
+    var value = ob[key];
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+
+      arr.push(key + "=" + value);
+    }
+  }
+  return arr.toString();
+}
+
 var orm = {
-    all: function(tableInput, cb) {
+    selectAll: function(tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + ";";
         connection.query(queryString, function(err, result) {
           if (err) {
@@ -11,14 +37,44 @@ var orm = {
         });
       },
 
-      //do I need to build these as functions or should I be following the logic above from the cats exercise?  
-    selectAll(),
-
-    insertOne();
+      insertOne: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
     
-    updateOne();
-
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+    
+        console.log(queryString);
+    
+        connection.query(queryString, vals, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      },
+      updateOne: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+    
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      },
 }
 
 
-module.exports
+module.exports = orm;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,6 @@
 INSERT INTO burgers(burger_name, devoured)
 VALUES ("Chili Cheese Burger", false), 
-("Bahn Mi BUrger", false), 
+("Bahn Mi Burger", false), 
 ("Pimento Burger", false), 
-("All American Burger", false)
+("All American Burger", false),
+("Nashville Hot Chicken Burger", false)
