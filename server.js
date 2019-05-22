06fbfcb810207a0ea67be21e356b6036d9bd04e8
rsuttles58 <robert.suttles58@gmail.com>
2019-05-21 21:56:41 -0500

    Made a path change in routing

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
